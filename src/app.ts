f4c992e9b561cedd7c6c41227d088d58430344ba
gn-GDS <george.nemeshanyi@digital.cabinet-office.gov.uk>
2024-07-02 14:09:10 +0100

    ATO-750: Setup linting and pre commit hook

--- src/app.ts
+++ src/app.ts
@@ -8,5 +8,5 @@ app.get("/", (req: Request, res: Response) => {
 });
 
 app.listen(port, () => {
-  console.log(`[server]: Server is running at http://localhost:${port}`);
+  console.log(`[server]: Server is running at http://localhost:${port}`); // eslint-disable-line
 });
