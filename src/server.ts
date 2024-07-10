d6702c38d0227e64d27e20931ca8f6cbcc855fc5
cargil <carly.gilson@digital.cabinet-office.gov.uk>
2024-07-10 11:24:18 +0100

    ATO-751: Set up example test

--- src/server.ts
+++ src/server.ts
@@ -3,7 +3,7 @@ import { createApp } from "./app";
 const port = process.env.PORT || 3000;
 
 (async () => {
-  const app = await createApp();
+  const app = createApp();
 
   app.listen(port, () => {
     console.log(`[server]: Server is running at http://localhost:${port}`); // eslint-disable-line
