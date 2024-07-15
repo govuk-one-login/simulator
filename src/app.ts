c96d22da1bf97df8880d59e9f83c45f72470918c
cargil <carly.gilson@digital.cabinet-office.gov.uk>
2024-07-15 14:56:38 +0100

    ATO-823: Set up body parser package

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,12 @@
 import express, { Application, Express, Request, Response } from "express";
+import bodyParser from "body-parser";
 
 const createApp = (): Application => {
   const app: Express = express();
+
+  app.use(express.json());
+  app.use(bodyParser.json());
+  app.use(bodyParser.urlencoded({ extended: false }));
 
   app.get("/", (req: Request, res: Response) => {
     res.send("Express + TypeScript Server");
