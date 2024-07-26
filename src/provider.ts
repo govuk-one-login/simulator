05a49e63bd7ef494e7cefb0e9df67a1bb6385096
Ryan Andrews <ryan.andrews@digital.cabinet-office.gov.uk>
2024-07-26 13:34:55 +0100

    ATO-827: Creates a oidc provider
    Creates an oidc provider using the parameters from the node-oidc-provider
    package. This tries to mirror our own oidc configuration[1] as closely
    as possible. This probably still needs a bit more work but is a start
    the provider uses the loadExsistingGrant function which is outlined
    in the documentation[1] and is adapted from some example code[2]
    
    [1] - https://github.com/panva/node-oidc-provider/tree/main/docs#interactions
    [2] - https://github.com/panva/node-oidc-provider/blob/main/recipes/skip_consent.md

---
+++ src/provider.ts
@@ -0,0 +1,147 @@
+import Provider, {
+  Configuration,
+  interactionPolicy,
+  KoaContextWithOIDC,
+} from "oidc-provider";
+import { Config } from "./config.js";
+import { createOidcClientFromConfig } from "./helper/create-oidc-client-from-config.js";
+import { claimsValidator } from "./validators/claims-validator.js";
+import { vtrValidator } from "./validators/vtr-validator.js";
+import { ISSUER_VALUE } from "./constants/provider-config.js";
+import { errorRender } from "./error/error-render.js";
+import { findAccount } from "./components/find-account.js";
+
+const { base } = interactionPolicy;
+
+const baseInteractions = base();
+//We do not use a consent prompt so this can be removed
+baseInteractions.remove("consent");
+
+const createOidcProvider = async (): Promise<Provider> => {
+  const config: Configuration = {
+    features: {
+      claimsParameter: {
+        enabled: true,
+        assertClaimsParameter: claimsValidator,
+      },
+      backchannelLogout: { enabled: true },
+      userinfo: { enabled: true },
+      pushedAuthorizationRequests: { enabled: false },
+      registration: {
+        enabled: false,
+      },
+      devInteractions: { enabled: false },
+    },
+    extraParams: {
+      vtr: vtrValidator,
+    },
+    routes: {
+      authorization: "/authorize",
+      token: "/token",
+      userinfo: "/userinfo",
+      end_session: "/logout",
+      jwks: "/.well-known/jwks.json",
+    },
+    responseTypes: ["code"],
+    claims: {
+      email: ["email", "email_verified"],
+      phone: ["phone_number", "phone_number_verified"],
+      openid: ["sub"],
+      wallet_subject_id: ["sub"],
+      "https://vocab.account.gov.uk/v1/passport": null,
+      "https://vocab.account.gov.uk/v1/socialSecurityRecord": null,
+      "https://vocab.account.gov.uk/v1/coreIdentityJWT": null,
+      "https://vocab.account.gov.uk/v1/address": null,
+      "https://vocab.account.gov.uk/v1/inheritedIdentityJWT": null,
+      "https://vocab.account.gov.uk/v1/drivingPermit": null,
+    },
+    scopes: ["openid", "email", "phone", "wallet_subject_id"],
+    subjectTypes: ["public", "pairwise"],
+    clientAuthMethods: ["private_key_jwt"],
+    clientDefaults: {
+      token_endpoint_auth_method: "private_key_jwt",
+      scope: "openid",
+      id_token_signed_response_alg: "ES256",
+      backchannel_logout_session_required: false,
+      response_types: ["code"],
+      identity_verification_supported: false,
+    },
+    enabledJWA: {
+      idTokenSigningAlgValues: ["RS256", "ES256"],
+      clientAuthSigningAlgValues: [
+        "RS256",
+        "RS384",
+        "RS512",
+        "PS256",
+        "PS384",
+        "PS512",
+      ],
+    },
+    allowOmittingSingleRegisteredRedirectUri: false,
+    clients: [await createOidcClientFromConfig(Config.getInstance())],
+    pkce: {
+      required: () => false,
+    },
+    discovery: {
+      ui_locales_supported: ["en", "cy"],
+      service_documentation: "https://docs.sign-in.service.gov.uk/",
+      op_policy_uri: "https://signin.integration.account.gov.uk/privacy-notice",
+      op_tos_uri:
+        "https://signin.integration.account.gov.uk/terms-and-conditions",
+      trustmarks: "https://oidc.integration.account.gov.uk/trustmark",
+    },
+    renderError: errorRender,
+    findAccount: findAccount,
+    loadExistingGrant: async (ctx: KoaContextWithOIDC) => {
+      const { oidc } = ctx;
+      //adapted from https://github.com/panva/node-oidc-provider/blob/main/recipes/skip_consent.md
+      const grantId =
+        oidc.result?.consent?.grantId ||
+        oidc?.session?.grantIdFor(oidc.client?.clientId as string);
+
+      if (grantId) {
+        const grant = await ctx.oidc.provider.Grant.find(grantId);
+
+        if (
+          grant &&
+          grant.exp &&
+          oidc.account &&
+          grant.exp < (oidc.session?.exp as number)
+        ) {
+          grant.exp = oidc.session?.exp;
+
+          await grant.save();
+        }
+
+        return grant;
+      } else {
+        const grant = new ctx.oidc.provider.Grant({
+          clientId: oidc.client?.clientId,
+          accountId: oidc.session?.accountId,
+        });
+
+        const claims = oidc.claims.userinfo;
+        const scopes = (oidc.params?.scope as string).split(" ");
+
+        scopes.forEach((scope) => grant.addOIDCScope(scope));
+
+        if (claims) {
+          grant.addOIDCClaims(Object.keys(claims));
+        }
+
+        await grant.save();
+        return grant;
+      }
+    },
+    interactions: {
+      policy: [...baseInteractions],
+    },
+    cookies: {
+      keys: ["some secret key"],
+    },
+  };
+
+  return new Provider(ISSUER_VALUE, config);
+};
+
+export { createOidcProvider };
