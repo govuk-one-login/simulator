fd67a69d11e0b63e9b0b2f50ac162de2cba2d4b4
Ryan Andrews <ryan.andrews@digital.cabinet-office.gov.uk>
2024-08-12 11:25:43 +0100

    ATO-827: Adds more tests
    Adds an additional tests for unknown redirect uri, unknown scope,
    and scope not in user configuration.

--- tests/integration/oidc.test.ts
+++ tests/integration/oidc.test.ts
@@ -1,7 +1,9 @@
+import { jest } from "@jest/globals";
 import { randomUUID } from "crypto";
 import { createApp } from "../../src/app";
 import request from "supertest";
 import { ISSUER_VALUE } from "../../src/constants/provider-config";
+import { Config } from "../../src/config";
 
 const testEnvVars = {
   CLIENT_ID: "TEST_CLIENT_ID",
@@ -19,7 +21,6 @@ const urlEncodeComponent = (key: string, value: string) =>
 const constructClientRequestUri = (params: Record<string, string>): string => {
   return (
     "/authorize?" +
-    urlEncodeComponent("response_type", "code") +
     Object.entries(params)
       .map(([key, val]) => urlEncodeComponent(key, val))
       .join("")
@@ -31,6 +32,10 @@ describe("oidc tests", () => {
     Object.entries(testEnvVars).map(([key, val]) => {
       process.env[key] = val;
     });
+  });
+
+  beforeEach(() => {
+    jest.restoreAllMocks();
   });
 
   it("redirects to redirect uri and returns an error for an invalid claim", async () => {
@@ -48,12 +53,38 @@ describe("oidc tests", () => {
       state,
       redirect_uri: testEnvVars.REDIRECT_URLS,
       scope: testEnvVars.SCOPES.split(",").join(" "),
+      response_type: "code",
     });
     const response = await request(app).get(requestUri);
     expect(response.status).toEqual(303);
     expect(response.header.location).toStrictEqual(
       testEnvVars.REDIRECT_URLS +
         `?error=invalid_request${urlEncodeComponent("error_description", "Request contains invalid claims")}${urlEncodeComponent("state", state)}${urlEncodeComponent("iss", ISSUER_VALUE)}`
+    );
+  });
+
+  it("redirects to redirect uri and with url encoded error for an invalid scope", async () => {
+    const state = randomUUID();
+    const app = await createApp();
+    const requestUri = constructClientRequestUri({
+      client_id: testEnvVars.CLIENT_ID,
+      claims: JSON.stringify({
+        userinfo: {
+          [testEnvVars.CLAIMS]: null,
+        },
+      }),
+      vtr: JSON.stringify(["Cl.Cm.P2"]),
+      nonce: (Math.random() * 1000).toString(),
+      state,
+      redirect_uri: testEnvVars.REDIRECT_URLS,
+      scope: ["openid", "unknown"].join(" "),
+      response_type: "code",
+    });
+    const response = await request(app).get(requestUri);
+    expect(response.status).toEqual(303);
+    expect(response.header.location).toStrictEqual(
+      testEnvVars.REDIRECT_URLS +
+        `?error=invalid_scope${urlEncodeComponent("error_description", "Invalid, unknown or malformed scope")}${urlEncodeComponent("scope", "unknown")}${urlEncodeComponent("state", state)}${urlEncodeComponent("iss", ISSUER_VALUE)}`
     );
   });
 
@@ -72,6 +103,7 @@ describe("oidc tests", () => {
       state,
       redirect_uri: testEnvVars.REDIRECT_URLS,
       scope: testEnvVars.SCOPES.split(",").join(" "),
+      response_type: "code",
     });
     const response = await request(app).get(requestUri);
     expect(response.status).toEqual(303);
@@ -96,6 +128,7 @@ describe("oidc tests", () => {
       state,
       redirect_uri: testEnvVars.REDIRECT_URLS,
       scope: testEnvVars.SCOPES.split(",").join(" "),
+      response_type: "code",
     });
     const response = await request(app).get(requestUri);
     expect(response.status).toEqual(400);
@@ -116,10 +149,60 @@ describe("oidc tests", () => {
       nonce: (Math.random() * 1000).toString(),
       state,
       scope: testEnvVars.SCOPES.split(",").join(" "),
+      response_type: "code",
     });
     const response = await request(app).get(requestUri);
     expect(response.status).toEqual(400);
     expect(response.text).toStrictEqual("Bad Request");
+  });
+
+  it("returns a 400 and Bad Request for an unknown redirect uri", async () => {
+    const state = randomUUID();
+    const app = await createApp();
+    const requestUri = constructClientRequestUri({
+      client_id: testEnvVars.CLIENT_ID,
+      claims: JSON.stringify({
+        userinfo: {
+          [testEnvVars.CLAIMS]: null,
+        },
+      }),
+      vtr: JSON.stringify(["Cl.Cm.P2"]),
+      nonce: (Math.random() * 1000).toString(),
+      state,
+      scope: testEnvVars.SCOPES.split(",").join(" "),
+      redirect_uri: "bad.redirect.example.com/auth-code-callback",
+      response_type: "code",
+    });
+    const response = await request(app).get(requestUri);
+    expect(response.status).toEqual(400);
+    expect(response.text).toStrictEqual("Bad Request");
+  });
+
+  it("returns a 303 and url encoded error for scope not in client config ", async () => {
+    jest.spyOn(Config.getInstance(), "getScopes").mockReturnValue(["openid"]);
+    const state = randomUUID();
+    const app = await createApp();
+    const requestUri = constructClientRequestUri({
+      client_id: testEnvVars.CLIENT_ID,
+      claims: JSON.stringify({
+        userinfo: {
+          [testEnvVars.CLAIMS]: null,
+        },
+      }),
+      vtr: JSON.stringify(["Cl.Cm.P2"]),
+      nonce: (Math.random() * 1000).toString(),
+      state,
+      scope: "openid phone",
+      redirect_uri: testEnvVars.REDIRECT_URLS,
+      response_type: "code",
+    });
+
+    const response = await request(app).get(requestUri);
+    expect(response.status).toEqual(303);
+    expect(response.header.location).toStrictEqual(
+      testEnvVars.REDIRECT_URLS +
+        `?error=invalid_scope${urlEncodeComponent("error_description", "requested scope is not allowed")}${urlEncodeComponent("scope", "phone")}${urlEncodeComponent("state", state)}${urlEncodeComponent("iss", ISSUER_VALUE)}`
+    );
   });
 
   it("redirects to the redirectUri with an auth code for a valid auth request", async () => {
@@ -137,9 +220,9 @@ describe("oidc tests", () => {
       scope: testEnvVars.SCOPES.split(",").join(" "),
       nonce: (Math.random() * 1000).toString(),
       redirect_uri: testEnvVars.REDIRECT_URLS,
+      response_type: "code",
     });
     const agent = request.agent(app);
-
     const authorizeResponse = await agent.get(requestUri);
     const loginInteractionRedirect = authorizeResponse.header.location;
     expect(authorizeResponse.status).toEqual(303);
