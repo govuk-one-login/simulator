330eb43e8c1645fdd64796edbd44652ad53da5e2
Ryan Andrews <ryan.andrews@digital.cabinet-office.gov.uk>
2024-08-22 09:46:36 +0100

    ATO-933: adds function to create id token

---
+++ src/components/token/helper/create-id-token.ts
@@ -0,0 +1,75 @@
+import { createHash } from "crypto";
+import { Config } from "../../../config";
+import {
+  ID_TOKEN_EXPIRY,
+  ISSUER_VALUE,
+  SESSION_ID,
+  TRUSTMARK_URL,
+} from "../../../constants";
+import { logger } from "../../../logger";
+import AuthRequestParameters from "src/types/auth-request-parameters";
+import { signToken } from "./sign-token";
+
+type IdTokenClaims = {
+  at_hash: string;
+  sub: string;
+  aud: string;
+  iss: string;
+  vot: string;
+  exp: number;
+  iat: number;
+  nonce: string;
+  vtm: string;
+  sid: string;
+};
+
+export const createIdToken = async (
+  authRequestParams: AuthRequestParameters,
+  accessToken: string
+): Promise<string> => {
+  logger.info("Creating Id token");
+
+  const clientConfig = Config.getInstance();
+  const idTokenClaims = createIdTokenClaimSet(
+    clientConfig,
+    authRequestParams,
+    accessToken
+  );
+
+  const signedIdToken = await signToken(idTokenClaims);
+
+  logger.info("Id token created");
+
+  return signedIdToken;
+};
+
+const createIdTokenClaimSet = (
+  clientConfig: Config,
+  authRequestParams: AuthRequestParameters,
+  accessToken: string
+): IdTokenClaims => {
+  const iat = Math.floor(Date.now() / 1000);
+  const exp = iat + ID_TOKEN_EXPIRY;
+
+  return {
+    iat,
+    exp,
+    at_hash: generateAccessTokenHash(accessToken),
+    sub: clientConfig.getSub(),
+    aud: clientConfig.getClientId(),
+    iss: ISSUER_VALUE,
+    sid: SESSION_ID,
+    vot: authRequestParams.vtr.credentialTrust,
+    nonce: authRequestParams.nonce,
+    vtm: TRUSTMARK_URL,
+  };
+};
+
+const generateAccessTokenHash = (accessToken: string): string => {
+  //This assumes our hashing algorithm will always be sha256
+  //given we only support RS256 and ES256 I think this is correct
+  const hash = createHash("sha256");
+  const hashedToken = hash.update(Buffer.from(accessToken, "ascii")).digest();
+  const firstHalf = hashedToken.subarray(0, Math.ceil(hashedToken.length / 2));
+  return firstHalf.toString("base64url");
+};
---
+++ src/components/token/tests/helper/create-id-token.test.ts
@@ -0,0 +1,110 @@
+import { Config } from "../../../../config";
+import {
+  ID_TOKEN_EXPIRY,
+  EC_KEY_ID,
+  ISSUER_VALUE,
+  RSA_KEY_ID,
+  TRUSTMARK_URL,
+  SESSION_ID,
+} from "../../../../constants";
+import AuthRequestParameters from "../../../../types/auth-request-parameters";
+import { createIdToken } from "../../helper/create-id-token";
+
+describe("createIdToken tests", () => {
+  const mockAuthRequestParams: AuthRequestParameters = {
+    claims: [],
+    nonce: "nonce-1238rhbh4r84=4rij=r4r",
+    scopes: ["openid"],
+    redirectUri: "https://example.com/authentication-callback/",
+    vtr: {
+      credentialTrust: "Cl.Cm",
+    },
+  };
+  const testTimestamp = 1723707024;
+  const testClientId = "testClientId";
+  const testSubClaim =
+    "urn:fdc:gov.uk:2022:56P4CMsGh_02YOlWpd8PAOI-2sVlB2nsNU7mcLZYhYw=";
+  const testAccessToken =
+    "eyJhbGciOiJSUzI1NiIsImtpZCI6IjczMzRiNzE4LTNmMjktNDRlZi04YjY1LWUyNjZhMTdkYWVhNSJ9.eyJleHAiOjkzNjQ4OTc4MSwiaWF0Ijo5MzY0ODk2MDEsImlzcyI6Imh0dHBzOi8vb2lkYy50ZXN0LmFjY291bnQuZ292LnVrIiwianRpIjoiMTIzNDU2NyIsImNsaWVudF9pZCI6InRlc3RDbGllbnRJZCIsInN1YiI6InVybjpmZGM6Z292LnVrOjIwMjI6NTZQNENNc0doXzAyWU9sV3BkOFBBT0ktMnNWbEIybnNOVTdtY0xaWWhZdz0iLCJzaWQiOiIxMjM0NTY3Iiwic2NvcGUiOlsib3BlbmlkIl19.GDDz3DcWSUCWMT8OkxZvU8ffiAjOKcNNaW23RzlEBer3G4Xz5Sp7moGtbP4vcfXT_pLUy-_YTIsiJ9r-A1gchhmx_qbfnWcqHxwj3DFYZ_Q16XgpB_7o_MtsiY1aAhqd8-zywTg25aczMHPtZMLVdYx9vw8zlF9iI9sOscS-s5Bje1yZ6ZmbHseHYVa8yJmZIjoKcdnGQXQwGQFp1KyzkA2gJxnR19Nc8O9oM4PA5y6uBCme3YTknei3T3tfJrPiBevtdvr9SV5fBTK2MrPzHao51_8nT841TdnMbHWxYp0FHTiBw7aAQO2VoKQ6Zku5CqBd3dyeQy_sZDNOFrDOTA";
+
+  const clientIdSpy = jest.spyOn(Config.getInstance(), "getClientId");
+  const subSpy = jest.spyOn(Config.getInstance(), "getSub");
+  const tokenSigningAlgorithmSpy = jest.spyOn(
+    Config.getInstance(),
+    "getIdTokenSigningAlgorithm"
+  );
+
+  const decodeTokenPart = (part: string) =>
+    JSON.parse(Buffer.from(part, "base64url").toString());
+
+  beforeEach(() => {
+    jest.useFakeTimers();
+    jest.setSystemTime(testTimestamp);
+  });
+
+  afterEach(() => {
+    jest.useRealTimers();
+  });
+
+  it("returns a signed Id token using RS256", async () => {
+    tokenSigningAlgorithmSpy.mockReturnValue("RS256");
+    subSpy.mockReturnValue(testSubClaim);
+    clientIdSpy.mockReturnValue(testClientId);
+
+    const IdToken = await createIdToken(mockAuthRequestParams, testAccessToken);
+    const tokenParts = IdToken.split(".");
+
+    const header = decodeTokenPart(tokenParts[0]);
+    const payload = decodeTokenPart(tokenParts[1]);
+
+    expect(tokenParts.length).toBe(3);
+    expect(header).toStrictEqual({
+      alg: "RS256",
+      kid: RSA_KEY_ID,
+    });
+    expect(payload).toStrictEqual({
+      iat: Math.floor(testTimestamp / 1000),
+      exp: Math.floor(testTimestamp / 1000) + ID_TOKEN_EXPIRY,
+      iss: ISSUER_VALUE,
+      aud: testClientId,
+      sub: testSubClaim,
+      sid: SESSION_ID,
+      at_hash: "oB7bgQoIL9clDcgMdS4Ydg",
+      vtm: TRUSTMARK_URL,
+      vot: "Cl.Cm",
+      nonce: mockAuthRequestParams.nonce,
+    });
+    expect(typeof tokenParts[2]).toBe("string");
+  });
+
+  it("returns a signed Id Token using ES256", async () => {
+    tokenSigningAlgorithmSpy.mockReturnValue("ES256");
+    subSpy.mockReturnValue(testSubClaim);
+    clientIdSpy.mockReturnValue(testClientId);
+
+    const idToken = await createIdToken(mockAuthRequestParams, testAccessToken);
+    const tokenParts = idToken.split(".");
+
+    const header = decodeTokenPart(tokenParts[0]);
+    const payload = decodeTokenPart(tokenParts[1]);
+
+    expect(tokenParts.length).toBe(3);
+    expect(header).toStrictEqual({
+      alg: "ES256",
+      kid: EC_KEY_ID,
+    });
+    expect(payload).toStrictEqual({
+      iat: Math.floor(testTimestamp / 1000),
+      exp: Math.floor(testTimestamp / 1000) + ID_TOKEN_EXPIRY,
+      iss: ISSUER_VALUE,
+      aud: testClientId,
+      sub: testSubClaim,
+      sid: SESSION_ID,
+      at_hash: "oB7bgQoIL9clDcgMdS4Ydg",
+      vtm: TRUSTMARK_URL,
+      vot: mockAuthRequestParams.vtr.credentialTrust,
+      nonce: mockAuthRequestParams.nonce,
+    });
+    expect(typeof tokenParts[2]).toBe("string");
+  });
+});
