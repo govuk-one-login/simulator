2d6a1c34695f60500e93fea647fa510e1e58bf7a
Ryan Andrews <ryan.andrews@digital.cabinet-office.gov.uk>
2024-09-03 10:44:49 +0100

    ATO-827: Adds Vtr validator
    This validates aand parses a set of VTR values included in the
    auth request and ensure each one is valid. This attempts to
    replicate our own vtr parsing and validation code[1]
    
    [1] - https://github.com/govuk-one-login/authentication-api/blob/main/shared/src/main/java/uk/gov/di/authentication/shared/entity/VectorOfTrust.java

---
+++ src/validators/tests/vtr-validator.test.ts
@@ -0,0 +1,206 @@
+import { Config } from "../../config";
+import { AuthoriseRequestError } from "../../errors/authorise-request-error";
+import { vtrValidator } from "../vtr-validator";
+
+describe("vtrValidator tests", () => {
+  const config = Config.getInstance();
+
+  const levelOfConfidenceSpy = jest.spyOn(config, "getClientLoCs");
+  const state = "4063a64d651d9077adee7c51a26e87e8";
+  const redirectUri = "https://example.com/authentication-callback";
+
+  it("returns the default credential trust and level of confidence when vtr is undefined", () => {
+    levelOfConfidenceSpy.mockReturnValue(["P0", "P2"]);
+
+    expect(vtrValidator(undefined, config, state, redirectUri)).toStrictEqual([
+      {
+        levelOfConfidence: null,
+        credentialTrust: "Cl.Cm",
+      },
+    ]);
+  });
+
+  it("throws an error when a vtr includes more than 1 identity component", () => {
+    levelOfConfidenceSpy.mockReturnValue(["P0", "P2"]);
+
+    expect(() =>
+      vtrValidator('["Cl.Cm.P2.P0"]', config, state, redirectUri)
+    ).toThrow(
+      new AuthoriseRequestError({
+        errorCode: "invalid_request",
+        errorDescription: "Request vtr not valid",
+        httpStatusCode: 302,
+        state,
+        redirectUri,
+      })
+    );
+  });
+
+  it("ignores any unknown values", () => {
+    levelOfConfidenceSpy.mockReturnValue(["P0", "P2"]);
+
+    expect(
+      vtrValidator('["Cl.Cm.P2.D4"]', config, state, redirectUri)
+    ).toStrictEqual([
+      {
+        credentialTrust: "Cl.Cm",
+        levelOfConfidence: "P2",
+      },
+    ]);
+  });
+
+  it("throws an error when a vtr includes an unsupported credential trust value", () => {
+    levelOfConfidenceSpy.mockReturnValue(["P0", "P2"]);
+
+    expect(() => vtrValidator('["Ch.P2"]', config, state, redirectUri)).toThrow(
+      new AuthoriseRequestError({
+        errorCode: "invalid_request",
+        errorDescription: "Request vtr not valid",
+        httpStatusCode: 302,
+        state,
+        redirectUri,
+      })
+    );
+  });
+
+  it("throws an error when a vtr includes an invalid level of identity confidence", () => {
+    levelOfConfidenceSpy.mockReturnValue(["P0", "P2"]);
+
+    expect(() =>
+      vtrValidator('["Cl.Cm.P3"]', config, state, redirectUri)
+    ).toThrow(
+      new AuthoriseRequestError({
+        errorCode: "invalid_request",
+        errorDescription: "Request vtr not valid",
+        httpStatusCode: 302,
+        state,
+        redirectUri,
+      })
+    );
+  });
+
+  it("throws an error when a vtr specified P2 confidence with an credential trust value of Cl", () => {
+    levelOfConfidenceSpy.mockReturnValue(["P0", "P2"]);
+
+    expect(() => vtrValidator('["Cl.P2"]', config, state, redirectUri)).toThrow(
+      new AuthoriseRequestError({
+        errorCode: "invalid_request",
+        errorDescription: "Request vtr not valid",
+        httpStatusCode: 302,
+        state,
+        redirectUri,
+      })
+    );
+  });
+
+  it("throws an error when a vtr specified P1 confidence with an credential trust value of Cl", () => {
+    levelOfConfidenceSpy.mockReturnValue(["P0", "P1", "P2"]);
+
+    expect(() => vtrValidator('["Cl.P1"]', config, state, redirectUri)).toThrow(
+      new AuthoriseRequestError({
+        errorCode: "invalid_request",
+        errorDescription: "Request vtr not valid",
+        httpStatusCode: 302,
+        state,
+        redirectUri,
+      })
+    );
+  });
+
+  it("throws an error when a vtr set includes both identity and non identity components", () => {
+    levelOfConfidenceSpy.mockReturnValue(["P0", "P2"]);
+
+    expect(() =>
+      vtrValidator('["Cl.Cm.P2", "Cl.Cm"]', config, state, redirectUri)
+    ).toThrow(
+      new AuthoriseRequestError({
+        errorCode: "invalid_request",
+        errorDescription: "Request vtr not valid",
+        httpStatusCode: 302,
+        state,
+        redirectUri,
+      })
+    );
+  });
+
+  it("throws an error when a vtr set includes levels of identity confidence not supported by the client", () => {
+    levelOfConfidenceSpy.mockReturnValue(["P2"]);
+
+    expect(() =>
+      vtrValidator('["Cl.Cm.P2", "Cl.Cm.P0"]', config, state, redirectUri)
+    ).toThrow(
+      new AuthoriseRequestError({
+        errorCode: "invalid_request",
+        errorDescription: "Request vtr not valid",
+        httpStatusCode: 302,
+        state,
+        redirectUri,
+      })
+    );
+  });
+
+  it("throws an error when a vtr set is an invalid JSON array", () => {
+    levelOfConfidenceSpy.mockReturnValue(["P0", "P2"]);
+
+    expect(() =>
+      vtrValidator('["Cl.Cm.P2", "Cl.Cm.P0"]]]', config, state, redirectUri)
+    ).toThrow(
+      new AuthoriseRequestError({
+        errorCode: "invalid_request",
+        errorDescription: "Request vtr not valid",
+        httpStatusCode: 302,
+        state,
+        redirectUri,
+      })
+    );
+  });
+
+  it("does not allow both identity and non-identity components in the vtr with a P0", () => {
+    levelOfConfidenceSpy.mockReturnValue(["P0", "P2"]);
+
+    expect(() =>
+      vtrValidator('["Cl.Cm.P2", "Cl.Cm.P0"]]]', config, state, redirectUri)
+    ).toThrow(
+      new AuthoriseRequestError({
+        errorCode: "invalid_request",
+        errorDescription: "Request vtr not valid",
+        httpStatusCode: 302,
+        state,
+        redirectUri,
+      })
+    );
+  });
+
+  it("does not allow both identity and non-identity implicitly", () => {
+    levelOfConfidenceSpy.mockReturnValue(["P0", "P2"]);
+
+    expect(() =>
+      vtrValidator('["Cl.Cm.P2", "Cl.Cm"]]]', config, state, redirectUri)
+    ).toThrow(
+      new AuthoriseRequestError({
+        errorCode: "invalid_request",
+        errorDescription: "Request vtr not valid",
+        httpStatusCode: 302,
+        state,
+        redirectUri,
+      })
+    );
+  });
+
+  it("returns a parsed valid vtr set", () => {
+    levelOfConfidenceSpy.mockReturnValue(["P1", "P2"]);
+
+    expect(
+      vtrValidator('["Cl.Cm.P2", "Cl.Cm.P1"]', config, state, redirectUri)
+    ).toStrictEqual([
+      {
+        levelOfConfidence: "P2",
+        credentialTrust: "Cl.Cm",
+      },
+      {
+        levelOfConfidence: "P1",
+        credentialTrust: "Cl.Cm",
+      },
+    ]);
+  });
+});
---
+++ src/validators/vtr-validator.ts
@@ -0,0 +1,134 @@
+import { Config } from "../config";
+import { AuthoriseRequestError } from "../errors/authorise-request-error";
+import { logger } from "../logger";
+import { VALID_CREDENTIAL_TRUST_VALUES, VALID_LOC_VALUES } from "../constants";
+import { VectorOfTrust } from "../types/vector-of-trust";
+
+export const vtrValidator = (
+  vtr: string | undefined,
+  config: Config,
+  state: string,
+  redirectUri: string
+): VectorOfTrust[] => {
+  if (!vtr) {
+    logger.info("No vtr value provided, attaching default credential trust");
+    return [
+      {
+        levelOfConfidence: null,
+        credentialTrust: "Cl.Cm",
+      },
+    ];
+  }
+
+  let vtrSet: string[];
+  let parsedVtrSet: VectorOfTrust[];
+
+  try {
+    vtrSet = JSON.parse(vtr);
+    parsedVtrSet = vtrSet.map(parseSingleVtr);
+  } catch (error) {
+    logger.error("Error parsing vtr value: " + (error as Error).message);
+    throw generateInvalidVtrError(redirectUri, state);
+  }
+
+  const clientLevelsOfConfidence = config.getClientLoCs();
+
+  const identityVectors = parsedVtrSet
+    .map((vector) => vector.levelOfConfidence)
+    .filter(
+      (levelOfConfidence) =>
+        levelOfConfidence !== null && levelOfConfidence !== "P0"
+    );
+
+  if (
+    identityVectors.length !== 0 &&
+    identityVectors.length < parsedVtrSet.length
+  ) {
+    logger.error("VTR cannot contain both identity and non-identity vectors");
+    throw generateInvalidVtrError(redirectUri, state);
+  }
+
+  if (!identityVectors.every((loc) => clientLevelsOfConfidence.includes(loc))) {
+    logger.error(
+      "Level of confidence values have been requested which this client is not permitted to request. Level of confidence values in request: " +
+        identityVectors.join(" ")
+    );
+    throw generateInvalidVtrError(redirectUri, state);
+  }
+
+  return parsedVtrSet;
+};
+
+const parseSingleVtr = (singleVtr: string): VectorOfTrust => {
+  const splitVtrValues = singleVtr.split(".");
+
+  const credentialTrustValue = splitVtrValues
+    .filter(isCredentialTrust)
+    .sort()
+    .join(".") as "Cl" | "Cl.Cm";
+
+  const providedLevelsOfConfidence = splitVtrValues.filter(isVectorOfTrust);
+  const unknownValues = splitVtrValues.filter(isUnknown);
+
+  if (
+    !providedLevelsOfConfidence.every((loc) => VALID_LOC_VALUES.includes(loc))
+  ) {
+    throw new Error("Invalid LevelOfConfidence provided");
+  }
+
+  if (providedLevelsOfConfidence.length > 1) {
+    throw new Error(
+      "VTR must contain either 0 or 1 identity proofing components"
+    );
+  }
+
+  if (unknownValues.length !== 0) {
+    logger.warn(
+      `Unknown values in VTR, these will be ignored: ${unknownValues}`
+    );
+  }
+
+  if (!VALID_CREDENTIAL_TRUST_VALUES.includes(credentialTrustValue)) {
+    throw new Error("Invalid Credential Trust Value provided");
+  }
+
+  const parsedVtr: VectorOfTrust = {
+    credentialTrust: credentialTrustValue,
+    levelOfConfidence: providedLevelsOfConfidence[0] ?? null,
+  };
+
+  if (
+    (parsedVtr.levelOfConfidence !== null ||
+      parsedVtr.levelOfConfidence !== "P0") &&
+    parsedVtr.credentialTrust !== "Cl.Cm"
+  ) {
+    throw new Error(
+      "Non zero identity confidence must require at least Cl.Cm credential trust"
+    );
+  }
+
+  return parsedVtr;
+};
+
+const isCredentialTrust = (
+  credentialTrust: string
+): credentialTrust is "Cl" | "Cm" => credentialTrust.startsWith("C");
+
+const isVectorOfTrust = (
+  levelOfConfidence: string
+): levelOfConfidence is "P0" | "P1" | "P2" => levelOfConfidence.startsWith("P");
+
+const isUnknown = (vtr: string): boolean =>
+  !isCredentialTrust(vtr) && !isVectorOfTrust(vtr);
+
+const generateInvalidVtrError = (
+  redirectUri: string,
+  state: string
+): AuthoriseRequestError =>
+  new AuthoriseRequestError({
+    errorCode: "invalid_request",
+    errorDescription: "Request vtr not valid",
+    httpStatusCode: 302,
+    state,
+    redirectUri,
+  });
