bac52d5eb1a67debf87a21f85c4f72d05a007065
Ryan Andrews <ryan.andrews@digital.cabinet-office.gov.uk>
2024-09-03 12:07:05 +0100

    ATO-827: remove await from createApp
    The function is not async so the await keyword is no needed

--- tests/integration/app.test.ts
+++ tests/integration/app.test.ts
@@ -3,7 +3,7 @@ import request from "supertest";
 
 describe("Integration: Healthcheck", () => {
   test("Healthcheck endpoint returns 200", async () => {
-    const app = await createApp();
+    const app = createApp();
     const response = await request(app).get("/");
     expect(response.status).toEqual(200);
     expect(response.text).toEqual("Express + TypeScript Server");
