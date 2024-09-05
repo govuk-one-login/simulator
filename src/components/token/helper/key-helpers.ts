0e2ed96b7525e52f9cb04a79edd22e46e6948fb0
Ethan Mills <ethan.mills@digital.cabinet-office.gov.uk>
2024-09-05 18:17:09 +0100

    sq

--- src/components/token/helper/key-helpers.ts
+++ src/components/token/helper/key-helpers.ts
@@ -1,10 +1,12 @@
-import { exportJWK, importPKCS8, KeyLike, type JSONWebKeySet } from "jose";
+import { exportJWK, importPKCS8, type JSONWebKeySet, JWK, KeyLike } from "jose";
+import { createPrivateKey, createPublicKey } from "node:crypto";
 import {
   EC_KEY_ID,
   EC_PRIVATE_TOKEN_SIGNING_KEY,
   RSA_KEY_ID,
   RSA_PRIVATE_TOKEN_SIGNING_KEY,
 } from "../../../constants";
+import { Buffer } from "node:buffer";
 
 export const getTokenSigningKey = (
   tokenSigningAlgorithm: string
@@ -25,13 +27,34 @@ export const getKeyId = (tokenSigningAlgorithm: string): string => {
 };
 
 export const generateJWKS = async (): Promise<JSONWebKeySet> => {
-  const ecKey = await importPKCS8(EC_PRIVATE_TOKEN_SIGNING_KEY, "EC");
-  const rsKey = await importPKCS8(RSA_PRIVATE_TOKEN_SIGNING_KEY, "RSA");
-  const ecJwk = await exportJWK(ecKey);
-  ecJwk.kid = EC_KEY_ID;
-  const rsJwk = await exportJWK(rsKey);
-  rsJwk.kid = RSA_KEY_ID;
+  const ecPubJwk = await publicJwkFromPrivateKey(
+    EC_PRIVATE_TOKEN_SIGNING_KEY,
+    EC_KEY_ID
+  );
+
+  const rsPubJwk = await publicJwkFromPrivateKey(
+    RSA_PRIVATE_TOKEN_SIGNING_KEY,
+    RSA_KEY_ID
+  );
   return {
-    keys: [ecJwk, rsJwk],
+    keys: [ecPubJwk, rsPubJwk],
   };
 };
+
+async function publicJwkFromPrivateKey(
+  privateKeyString: string,
+  kid: string
+): Promise<JWK> {
+  const privateKey = createPrivateKey({
+    key: Buffer.from(
+      privateKeyString.replace(/-----(?:BEGIN|END) PRIVATE KEY-----|\s/g, ""),
+      "base64"
+    ),
+    type: "pkcs8",
+    format: "der",
+  });
+  const publicKey = createPublicKey(privateKey);
+  const jwk = await exportJWK(publicKey);
+  jwk.kid = kid;
+  return jwk;
+}
