7decca28acf83fbd7eaa8aead86a538c93faa026
Ryan Andrews <ryan.andrews@digital.cabinet-office.gov.uk>
2025-02-19 16:44:32 +0000

    ATO-1499: Adds body helpers for form config
    Adds some small validation helpers which validate the submitted
    form configuration

--- src/types/util/body-helpers.ts
+++ src/types/util/body-helpers.ts
@@ -1,4 +1,5 @@
 import { body, ValidationChain } from "express-validator";
+import { logger } from "../../logger";
 
 export const bodyRequired = (path: string): ValidationChain => {
   const substrings = path.split(".");
@@ -12,4 +13,34 @@ export const bodyOptional = (path: string): ValidationChain => {
 
 export const bodyOptionalAllowNull = (path: string): ValidationChain => {
   return body(path).optional({ values: "null" });
+};
+
+export const bodyOptionalAllowEmptyFormSubmission = (
+  path: string
+): ValidationChain => {
+  return body(path).optional({ values: "falsy" });
+};
+
+export const isOptionalJsonArray = (val: string): boolean => {
+  try {
+    return val ? Array.isArray(JSON.parse(val)) : true;
+  } catch (error) {
+    logger.error("Invalid JSON: " + (error as Error).message);
+
+    return false;
+  }
+};
+
+export const isOptionalJsonObject = (val: string): boolean => {
+  if (!val) {
+    return true;
+  } else {
+    try {
+      const parsedJson = JSON.parse(val);
+      return !Array.isArray(parsedJson) && typeof parsedJson === "object";
+    } catch (error) {
+      logger.error("Invalid JSON: " + (error as Error).message);
+      return false;
+    }
+  }
 };
