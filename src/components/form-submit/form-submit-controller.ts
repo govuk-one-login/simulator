398c20fba66c57d5ba9301578258a4e3b7b61a00
Ryan Andrews <ryan.andrews@digital.cabinet-office.gov.uk>
2025-02-21 12:37:34 +0000

    ATO-1449: Add validation to form field
    Adds validation to the submitted form fields and
    also updates the storing to store the parsed
    response config as previously we just stored the
    string value

--- src/components/form-submit/form-submit-controller.ts
+++ src/components/form-submit/form-submit-controller.ts
@@ -3,35 +3,71 @@ import { Config } from "../../config";
 import { base64url } from "jose";
 import AuthRequestParameters from "src/types/auth-request-parameters";
 import ResponseConfiguration from "src/types/response-configuration";
+import { validationResult } from "express-validator";
+import { logger } from "../../logger";
 
 export const formSubmitController = (req: Request, res: Response): void => {
-  const config = Config.getInstance();
-  const formBody = req.body;
-  const authCode = formBody.authCode;
-  const authRequestParams: AuthRequestParameters = JSON.parse(
-    Buffer.from(base64url.decode(formBody.authRequestParams)).toString()
-  );
+  try {
+    const errors = validationResult(req);
 
-  const responseConfiguration: ResponseConfiguration = {
-    sub: formBody.sub,
-    email: formBody.email,
-    emailVerified: formBody.emailVerified === "true",
-    phoneNumber: formBody.phoneNumber,
-    phoneNumberVerified: formBody.phoneNumberVerified === "true",
-    maxLoCAchieved: formBody.maxLoCAchieved,
-    coreIdentityVerifiableCredentials:
-      formBody.coreIdentityVerifiableCredentials,
-    passportDetails: formBody.passportDetails,
-    drivingPermitDetails: formBody.drivingPermitDetails,
-    postalAddressDetails: formBody.postalAddressDetails,
-    returnCodes: formBody.returnCodes,
-  };
+    if (!errors.isEmpty()) {
+      logger.error(errors);
+      const formattedErrors = errors
+        .array()
+        .map((err: any) => ({ field: err.path, message: err.msg }));
+      res.status(400);
+      res.json({
+        error: "Invalid request",
+        invalid_fields: formattedErrors,
+      });
+      return;
+    }
+    const config = Config.getInstance();
+    const formBody = req.body;
+    const authCode = formBody.authCode;
+    const authRequestParams: AuthRequestParameters = JSON.parse(
+      Buffer.from(base64url.decode(formBody.authRequestParams)).toString()
+    );
 
-  config.addToAuthCodeRequestParamsStore(authCode, {
-    ...authRequestParams,
-    responseConfiguration,
-  });
-  res.redirect(
-    `${authRequestParams.redirectUri}?code=${formBody.authCode}&state=${formBody.state}`
-  );
+    const responseConfiguration: ResponseConfiguration = {
+      sub: formBody.sub,
+      email: formBody.email ? formBody.email : null,
+      emailVerified: formBody.emailVerified === "true",
+      phoneNumber: formBody.phoneNumber,
+      phoneNumberVerified: formBody.phoneNumberVerified === "true",
+      maxLoCAchieved: formBody.maxLoCAchieved,
+      coreIdentityVerifiableCredentials: JSON.parse(
+        formBody.coreIdentityVerifiableCredentials
+      ),
+      passportDetails: formBody.passportDetails
+        ? JSON.parse(formBody.passportDetails)
+        : null,
+      drivingPermitDetails: formBody.drivingPermitDetails
+        ? JSON.parse(formBody.drivingPermitDetails)
+        : null,
+      postalAddressDetails: formBody.postalAddressDetails
+        ? JSON.parse(formBody.postalAddressDetails)
+        : null,
+      returnCodes: formBody.returnCodes
+        ? JSON.parse(formBody.returnCodes)
+        : null,
+    };
+
+    config.addToAuthCodeRequestParamsStore(authCode, {
+      ...authRequestParams,
+      responseConfiguration,
+    });
+    res.redirect(
+      `${authRequestParams.redirectUri}?code=${formBody.authCode}&state=${formBody.state}`
+    );
+  } catch (error) {
+    logger.error(
+      "Failed to put form configuration: ",
+      (error as Error).message
+    );
+    res.status(400);
+    res.json({
+      error: "Invalid request: Invalid JSON",
+    });
+  }
 };
