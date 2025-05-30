153a6fb92fc347d30a27c2e78ad6848944585744
Ryan Andrews <ryan.andrews@digital.cabinet-office.gov.uk>
2025-05-30 19:02:46 +0100

    ATO-1397: Configure and validate CoreIdentityJWT for simulator
    Adds the neccesary validation and config to setup the coreIdentityJWT
    for the simulator and apply the same validation as the in the RP case

--- tests/acceptance/data/identity.js
+++ tests/acceptance/data/identity.js
@@ -1,10 +1,12 @@
 const { AUTH_ONLY_REQUEST, AUTH_ONLY_RESPONSE } = require("./auth-only");
 const { PASSPORT } = require("./passport");
 const { ADDRESS } = require("./address");
+const { CORE_IDENTITY_VC_CLAIM } = require("./core-identity-vc");
 
 const IDENTITY_REQUEST = { ...AUTH_ONLY_REQUEST,
     passportDetails: PASSPORT,
     postalAddressDetails: ADDRESS,
+    coreIdentityVerifiableCredentials: CORE_IDENTITY_VC_CLAIM
 }
 
 const IDENTITY_RESPONSE = {
--- tests/acceptance/step_definitions/simulator-step-def.js
+++ tests/acceptance/step_definitions/simulator-step-def.js
@@ -2,6 +2,7 @@ const { When, Then } = require("@cucumber/cucumber");
 const { deepStrictEqual } = require("node:assert");
 const { AUTH_ONLY_REQUEST, AUTH_ONLY_RESPONSE } = require("../data/auth-only");
 const { IDENTITY_REQUEST, IDENTITY_RESPONSE } = require("../data/identity");
+const { validateCoreIdentityJwt } = require("../util/validate-core-identity-jwt");
 
 When("the simulator is sent the configuration", async function () {
     const configRequestOptions = {
@@ -44,11 +45,15 @@ When("the simulator is sent the identity configuration", async function () {
 })
 
 Then("the simulator returns the expected auth-only user info", async function () {
-    const authorizeResponse = await fetch('http://localhost:3000/authorize?vtr=%5B%22Cl.Cm%22%5D&scope=openid+email+phone&response_type=code&redirect_uri=http%3A%2F%2Flocalhost%3A3001%2Fcallback&state=QL1o9IKHyfTr4BpTCiMeROYKyd-8-k6vytO8OaUZspI&prompt=none&nonce=61SGsT-UYLpgIS2DmBKP-JUkMiqJx1jhe6mk8RpWjRQ&client_id=HGIOgho9HIRhgoepdIOPFdIUWgewi0jw');
+    const authorizeResponse = await fetch(`http://localhost:3000/authorize?vtr=%5B%22Cl.Cm%22%5D&scope=openid+email+phone&response_type=code&redirect_uri=http%3A%2F%2Flocalhost%3A3001%2Fcallback&state=QL1o9IKHyfTr4BpTCiMeROYKyd-8-k6vytO8OaUZspI&prompt=none&nonce=61SGsT-UYLpgIS2DmBKP-JUkMiqJx1jhe6mk8RpWjRQ&client_id=${process.env.RP_CLIENT_ID}`);
     deepStrictEqual(await authorizeResponse.json(), AUTH_ONLY_RESPONSE);
 });
 
 Then("the simulator returns the expected identity user info", async function () {
-    const authorizeResponse = await fetch('http://localhost:3000/authorize?vtr=%5B%22P2.Cl.Cm%22%5D&scope=openid+email+phone&claims=%7B%22userinfo%22%3A%7B%22https%3A%5C%2F%5C%2Fvocab.account.gov.uk%5C%2Fv1%5C%2Fpassport%22%3A%7B%22essential%22%3Atrue%7D%2C%22https%3A%5C%2F%5C%2Fvocab.account.gov.uk%5C%2Fv1%5C%2Faddress%22%3A%7B%22essential%22%3Atrue%7D%7D%7D&response_type=code&redirect_uri=http%3A%2F%2Flocalhost%3A3001%2Fcallback&state=QL1o9IKHyfTr4BpTCiMeROYKyd-8-k6vytO8OaUZspI&prompt=none&nonce=61SGsT-UYLpgIS2DmBKP-JUkMiqJx1jhe6mk8RpWjRQ&client_id=HGIOgho9HIRhgoepdIOPFdIUWgewi0jw');
-    deepStrictEqual(await authorizeResponse.json(), IDENTITY_RESPONSE);
+    const authorizeResponse = await fetch(`http://localhost:3000/authorize?vtr=%5B%22P2.Cl.Cm%22%5D&scope=openid+email+phone&claims=%7B%22userinfo%22%3A%7B%22https%3A%5C%2F%5C%2Fvocab.account.gov.uk%5C%2Fv1%5C%2Fpassport%22%3A%7B%22essential%22%3Atrue%7D%2C%22https%3A%5C%2F%5C%2Fvocab.account.gov.uk%5C%2Fv1%5C%2FcoreIdentityJWT%22%3A%7B%22essential%22%3Atrue%7D%2C%22https%3A%5C%2F%5C%2Fvocab.account.gov.uk%5C%2Fv1%5C%2Faddress%22%3A%7B%22essential%22%3Atrue%7D%7D%7D&response_type=code&redirect_uri=http%3A%2F%2Flocalhost%3A3001%2Fcallback&state=QL1o9IKHyfTr4BpTCiMeROYKyd-8-k6vytO8OaUZspI&prompt=none&nonce=61SGsT-UYLpgIS2DmBKP-JUkMiqJx1jhe6mk8RpWjRQ&client_id=${process.env.RP_CLIENT_ID}`);
+    const userinfo = await authorizeResponse.json();
+    const coreIdentityJWT = userinfo["https://vocab.account.gov.uk/v1/coreIdentityJWT"];
+    delete userinfo["https://vocab.account.gov.uk/v1/coreIdentityJWT"];
+    deepStrictEqual(userinfo, IDENTITY_RESPONSE);
+    await validateCoreIdentityJwt(coreIdentityJWT, false)
 });
