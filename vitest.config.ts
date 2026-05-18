0d2d28a8ce13116475fef54b1f9a5dea9cd10cf1
Craig Earl <craig.earl@digital.cabinet-office.gov.uk>
2026-05-18 14:15:16 +0100

    BAU: Add integration tests to vitest config
    - This was not actually running the integration tests before.

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,9 +2,9 @@ import { defineConfig } from "vitest/config";
 
 export default defineConfig({
   test: {
-    include: ["src/**/*.test.ts"],
+    include: ["src/**/*.test.ts", "tests/integration/*.test.ts"],
     coverage: {
-      include: ["src/**/*.ts"],
+      include: ["src/**/*.ts", "tests/integration/*.test.ts"],
       reporter: ["lcov", "text"],
     },
     globals: true,
